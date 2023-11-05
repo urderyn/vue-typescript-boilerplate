05ae52fd4112c69ef9fdcf1a310bbce00d135c89
urderyn <pau.segues@gmail.com>
2023-11-05 13:27:26 +0100

    feat(TypeScript) Typing Component Events 1:

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,16 @@
 import TheHeader from "@/components/TheHeader.vue";
 import EntryEditor from "./components/EntryEditor.vue";
 import EntryCard from "@/components/EntryCard.vue";
+import type Emoji from "@/types/Emoji";
+const handleCreateEntry = ( entry: { text: string, emoji: Emoji | null } ) => {
+
+}
 </script>
 
 <template>
   <main class="container m-auto p-10">
     <TheHeader />
-    <EntryEditor />
+    <EntryEditor @@create="handleCreateEntry"/>
     <ul>
       <li>
         <EntryCard />
--- src/components/EntryEditor.vue
+++ src/components/EntryEditor.vue
@@ -4,10 +4,18 @@ import ArrowCircleRight from "@/assets/icons/arrow-circle-right.svg";
 import type Emoji from "@/types/Emoji";
 import {ref, computed } from "vue";
 
+// data
 const text = ref("");
 const emoji = ref<Emoji | null>(null);
 const charCount = computed<number>(() => text.value.length);
 const maxChars = 280;
+
+// events
+defineEmits<{
+  (e: "@create", entry: { text: string, emoji: Emoji | null}): void
+}>();
+
+// methods
 const handleTextInput = (e: Event) => {
   const textarea = e.target as HTMLTextAreaElement;
   if (textarea.value <= maxChars) {
@@ -16,9 +24,10 @@ const handleTextInput = (e: Event) => {
     text.value = textarea.value = textarea.value.substring(0, maxChars)
   }
 }
+
 </script>
 <template>
-  <form class="entry-form" @submit.prevent>
+  <form class="entry-form" @submit.prevent="$emit('@create', {text, emoji})">
     <textarea :value="text" @keyup="handleTextInput" placeholder="New Journal Entry for danielkelly_io"></textarea>
     <EmojiField v-model="emoji"/>
     <div class="entry-form-footer">
