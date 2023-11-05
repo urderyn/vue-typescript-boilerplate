5ceeca3919d94a9c4a9731bffdc9e481d266e789
urderyn <pau.segues@gmail.com>
2023-11-05 12:21:46 +0100

    feat(TypeScript) Creating Types to reuse interfaces:

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,8 @@ import TheHeader from "@/components/TheHeader.vue";
 import EntryEditor from "./components/EntryEditor.vue";
 import EntryCard from "@/components/EntryCard.vue";
 import {reactive} from "vue";
-interface User {
-  id: number,
-  userName: string,
-  settings: string[]
-}
+import type User from "@/types/User";
+
 const user: User = reactive({
   id: 1,
   userName: 'miaumiau',
