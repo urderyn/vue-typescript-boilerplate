51bc24977076742d8b504845b26ae868a89822f7
urderyn <pau.segues@gmail.com>
2023-11-05 12:38:45 +0100

    feat(TypeScript) Creating union type Emoji:

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,9 @@
 import TheHeader from "@/components/TheHeader.vue";
 import EntryEditor from "./components/EntryEditor.vue";
 import EntryCard from "@/components/EntryCard.vue";
-import {reactive} from "vue";
-import type User from "@/types/User";
-
-const user: User = reactive({
-  id: 1,
-  userName: 'miaumiau',
-  settings: []
-})
-console.log(user.id)
 </script>
 
 <template>
-  {{user.userName}}
   <main class="container m-auto p-10">
     <TheHeader />
     <EntryEditor />
--- src/components/EntryEditor.vue
+++ src/components/EntryEditor.vue
@@ -1,10 +1,17 @@
 <script lang="ts" setup>
 import EmojiField from "@/components/EmojiField.vue";
 import ArrowCircleRight from "@/assets/icons/arrow-circle-right.svg";
+import type Emoji from "@/types/Emoji";
+import {Ref, ref} from "vue";
+
+const text = ref("");
+const emoji: Ref<Emoji | null>= ref(null);
+emoji.value = 'fooo';
+
 </script>
 <template>
   <form class="entry-form" @submit.prevent>
-    <textarea placeholder="New Journal Entry for danielkelly_io"></textarea>
+    <textarea v-model="ref" placeholder="New Journal Entry for danielkelly_io"></textarea>
     <EmojiField />
     <div class="entry-form-footer">
       <span>0 / 280</span>
